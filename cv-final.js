068a8122a694681ad0b72f9b5732ee8a3c02e138
LucasArismendi <123397981+LucasArismendi@users.noreply.github.com>
2023-01-27 18:50:02 -0300

    Add files via upload

---
+++ cv-final.js
@@ -0,0 +1,35 @@
+function mostrarCocacola(){
+	var x = document.getElementById("cocacola");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function mostrarDanone(){
+	var x = document.getElementById("danone");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function mostrarCoto(){
+	var x = document.getElementById("coto");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function mostrarMostaza(){
+	var x = document.getElementById("mostaza");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
